52473d7ee14ce347cebf3ac39bfc9e5bca444a0a
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-05-09 21:49:34 +0530

    get req + refactoring

--- http-request-project/src/send-request.py
+++ http-request-project/src/send-request.py
@@ -1,17 +1,35 @@
+import pprint
+
 import requests
 
+BASE_URL: str = 'https://httpbin.org'
 
-def go():
-    payload = { 'foo': 'bar' }
-    r = requests.post(
-        'https://httpbin.org/post',
-        json=payload
-    )
+
+def handle_response(r):
     if r.status_code == requests.codes.ok:
-        print(r.json())
+        pprint.pprint(r.json())
     else:
         print('something went wrong')
 
 
+def post():
+    payload = {'foo': 'bar'}
+    r = requests.post(
+        f'{BASE_URL}/post',
+        json=payload
+    )
+    handle_response(r)
+
+
+def get():
+    query_string = {'foo': 'bar'}
+    r = requests.get(
+        f'{BASE_URL}/get',
+        params=query_string
+    )
+    handle_response(r)
+
+
 if __name__ == '__main__':
-    go()
+    post()
+    get()
